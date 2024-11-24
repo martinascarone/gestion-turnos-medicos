eb092644c940ed7c71a6a77a00a2262f00637ba2
zoepreititasat <zoepreititasat@gmail.com>
2024-11-23 21:44:08 -0300

    preliminar

--- main.py
+++ main.py
@@ -1,152 +1,34 @@
+import json
+
 def main():
-    profesionales = [
-        {
-            "id": "1",
-            "nombre": "Carlos",
-            "apellido": "Maslaton",
-            "especialidad": "Oftalmologo",
-        },
-        {
-            "id": "2",
-            "nombre": "Juan",
-            "apellido": "Rodriguez",
-            "especialidad": "Oftalmologo",
-        },
-        {
-            "id": "3",
-            "nombre": "Carla",
-            "apellido": "Gomez",
-            "especialidad": "Pediatra",
-        },
-        {
-            "id": "4",
-            "nombre": "Martina",
-            "apellido": "Quintana",
-            "especialidad": "Nutricionista",
-        },
-        {
-            "id": "5",
-            "nombre": "Jorge",
-            "apellido": "Bisbal",
-            "especialidad": "Pediatra",
-        },
-        {
-            "id": "6",
-            "nombre": "Laura",
-            "apellido": "Perez",
-            "especialidad": "Oftalmologo",
-        },
-        {
-            "id": "7",
-            "nombre": "Mariana",
-            "apellido": "Isola",
-            "especialidad": "Nutricionista",
-        }
-    ]
-
-    disponibilidades = [
-        {
-            "idMedico": "1",
-            "dia": "Lunes",
-            "horarioEntrada": 13,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "1",
-            "dia": "Miércoles",
-            "horarioEntrada": 13,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "1",
-            "dia": "Jueves",
-            "horarioEntrada": 13,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "2",
-            "dia": "Martes",
-            "horarioEntrada": 13,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "2",
-            "dia": "Viernes",
-            "horarioEntrada": 13,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "2",
-            "dia": "Sábado",
-            "horarioEntrada": 13,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "3",
-            "dia": "Lunes",
-            "horarioEntrada": 10,
-            "horarioSalida": 13,
-        },
-        {
-            "idMedico": "3",
-            "dia": "Martes",
-            "horarioEntrada": 14,
-            "horarioSalida": 18,
-        },
-        {
-            "idMedico": "4",
-            "dia": "Jueves",
-            "horarioEntrada": 9,
-            "horarioSalida": 13,
-        },
-        {
-            "idMedico": "4",
-            "dia": "Lunes",
-            "horarioEntrada": 10,
-            "horarioSalida": 13,
-        },
-        {
-            "idMedico": "4",
-            "dia": "Miércoles",
-            "horarioEntrada": 10,
-            "horarioSalida": 12,
-        },
-        {
-            "idMedico": "5",
-            "dia": "Martes",
-            "horarioEntrada": 9,
-            "horarioSalida": 12,
-        },
-        {
-            "idMedico": "5",
-            "dia": "Jueves",
-            "horarioEntrada": 14,
-            "horarioSalida": 17,
-        },
-        {
-            "idMedico": "6",
-            "dia": "Viernes",
-            "horarioEntrada": 9,
-            "horarioSalida": 13,
-        },
-        {
-            "idMedico": "7",
-            "dia": "Viernes",
-            "horarioEntrada": 10,
-            "horarioSalida": 13,
-        }
-    ]
-
+    ARCHIVO_PACIENTE = "pacientes.csv"
+    ARCHIVO_TURNOS = "turnos.csv"
+    ARCHIVO_PROFESIONALES = "profesionales.json"
+    ARCHIVO_DISPONIBILIDADES = "disponibilidades.json"
+    
+    
+    def cargarProfesionales():
+        archivoProfesionales = open(ARCHIVO_PROFESIONALES , "rt")
+        return json.load(archivoProfesionales)
+    
+    def cargarDisponibilidades():
+        archivoDisponibilidades = open(ARCHIVO_DISPONIBILIDADES , "rt")
+        return json.load(archivoDisponibilidades)
+ 
     turnos = []
 
     pacientes = []
     
-    #def modificarTurno():
-    
     #def eliminarTurno():
     
     #def visualizarTurnos():
-    
+
+    def abrirArchivo(archivo):
+        # Intenta abrir archivo, si no existe lo crea
+        try:
+            return open(archivo , "a")
+        except OSError :
+                return open(archivo , "x")
 
     def isTurnoOcupado(dia, hora, idMedico):
         for turno in turnos:
@@ -157,6 +39,7 @@ def main():
     def imprimirTurnosDisponibles(profesional):
         contador = 0
         opciones = []
+        disponibilidades = cargarDisponibilidades()
 
         print("┌" + ("─"*4) + "┬" + ("─"*12) + "┬" + ("─"*10) + "┬" + ("─"*15) + "┐")
         print("│" + " " + "   " + "│" + " " + "Día" + " " * 8 + "│" + " " + "Horario" + "  " + "│" + " " + "Disponibilidad" +"│")
@@ -180,40 +63,7 @@ def main():
                     contador += 1
 
         print("└" + "─"*4 + "┴" + "─"*12 + "┴" + "─"*10 + "┴" + "─"*15 + "┘")
-
-        while True:
-            turnoSeleccionado = input("Ingrese el número del turno que desea seleccionar: ")
-            if turnoSeleccionado.isdigit():
-                turnoSeleccionado = int(turnoSeleccionado) - 1
-                if turnoSeleccionado < 0 or turnoSeleccionado >= len(opciones):
-                    print("Selección inválida. Por favor, seleccione un número válido.")
-                    continue
-                if opciones[turnoSeleccionado]["ocupado"]:
-                    print("El turno seleccionado está ocupado. Por favor, seleccione otro turno.")
-                else:
-                    return opciones[turnoSeleccionado]["dia"], opciones[turnoSeleccionado]["hora"]
-            else:
-                print("Debe ingresar un número válido.")
-        contador = 0
-        opciones = []
-        for turno in disponibilidades:
-            if turno["idMedico"] == str(profesional):
-                contadorHoras = turno["horarioSalida"] - turno["horarioEntrada"]
-                for hora in range(contadorHoras):
-                    dia = turno["dia"]
-                    hora_actual = turno["horarioEntrada"] + hora
-                    ocupado = isTurnoOcupado(dia, hora_actual, profesional)
-                    opciones.append({
-                        "dia": dia,
-                        "hora": hora_actual,
-                        "ocupado": ocupado
-                    })
-                    if ocupado:
-                        print("┌────────────────────────────────────────────────┐\n" + "│")
-                        print(contador + 1, ")", dia, hora_actual, "hs OCUPADO")
-                    else:
-                        print(contador + 1, ")", dia, hora_actual, "hs")
-                    contador += 1
+        
         while True:
             turnoSeleccionado = input("Ingrese el número del turno que desea seleccionar: ")
             if turnoSeleccionado.isdigit():
@@ -228,12 +78,15 @@ def main():
             else:
                 print("Debe ingresar un número válido.")
 
-
-    def seleccionarTurno(idTurno, dia, hora, paciente):
+    def guardarImprimirTurno(idTurno, dia, hora, dniPaciente):
+        archivo_turnos = abrirArchivo(ARCHIVO_TURNOS)
+        #TERMINARRRRRRRRRRRRRRRRRRRR
+        
+        archivo_turnos.write(dniPaciente + ',')
         turnos.append(
             {
                 "id": str(len(turnos) + 1),
-                "idPaciente": int(paciente),
+                "dni": int(dniPaciente),
                 "idMedico": int(idTurno),
                 "dia": dia,
                 "hora": hora
@@ -241,8 +94,6 @@ def main():
         print("\n-------------------------------------------------" )
         print(f"Turno reservado para el día {dia} a las {hora}:00 hs")
         print("-------------------------------------------------\n" )
-
-
 
     def generarListaEspecialidades(lista):
     #ingresa lista de diccionarios con datos de profesionales, sale listado de especialidades sin duplicados
@@ -256,7 +107,7 @@ def main():
 
 
     def imprimirEspecialidades(lista):
-    #imprime toda las especialidades disponibles
+    # Imprime toda las especialidades disponibles
         especialidades = generarListaEspecialidades(lista)
         indice = 0
         print("Hola! ¿Qué especialidad estás buscando?")
@@ -290,9 +141,8 @@ def main():
             print(indice + 1, ")", profesionales[indice]["nombre"], profesionales[indice]["apellido"])
             indice += 1
 
-
-
     def solicitarDatos():
+        pacientes_archivo = abrirArchivo(ARCHIVO_PACIENTE)
     #solicita los datos del paciente y valida entradas
         nombre = input("Ingrese su nombre: ")
         while not nombre.isalpha():
@@ -309,17 +159,15 @@ def main():
             print("El DNI debe contener solo números.")
             dni = input("Ingrese su DNI: ")
 
-        id = len(pacientes) + 1
-        pacientes.append([id, nombre, apellido, dni])
-        return id
-
-
+        pacientes_archivo.write(nombre + ',' + apellido + ',' + dni + '\n')
+        pacientes_archivo.close()
+        return dni
 
     # Inicio Programa
     especialidadSeleccionada = 0 
 
     while especialidadSeleccionada != -1:
-        especialidades = imprimirEspecialidades(profesionales)
+        especialidades = imprimirEspecialidades(cargarProfesionales())
 
         while True:
             try:
@@ -341,7 +189,7 @@ def main():
             break
 
         especialidadBuscada = especialidades[especialidadSeleccionada]
-        imprimirProfesionales(profesionales, especialidadBuscada)
+        imprimirProfesionales(cargarProfesionales(), especialidadBuscada)
 
 
         while True:
@@ -357,8 +205,8 @@ def main():
                 print("Error: Debe ingresar un número válido.")
 
         dia, hora = imprimirTurnosDisponibles(profesionalSeleccionado + 1)
-
-        seleccionarTurno(
+        print(dia, hora)
+    guardarImprimirTurno(
             profesionalSeleccionado + 1,
             dia,
             hora,
