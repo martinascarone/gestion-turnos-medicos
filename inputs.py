29382950004ccb440898478959d8404a4cf1031b
zoepreititasat <zoepreititasat@gmail.com>
2024-11-26 00:01:29 -0300

    autenticacion de usuario

--- inputs.py
+++ inputs.py
@@ -1,26 +1,45 @@
 
 def solicitarDatos():
-    #solicita los datos del paciente y valida entradas
-    nombre = input("Ingrese su nombre: ")
-    while not nombre.isalpha():
-        print("El nombre debe contener solo letras.")
-        nombre = input("Ingrese su nombre: ")
-
-    apellido = input("Ingrese su apellido: ")
-    while not apellido.isalpha():
-        print("El apellido debe contener solo letras.")
-        apellido = input("Ingrese su apellido: ")
-
+    # Solicita los datos del paciente y valida entradas
+    
     dni = input("Ingrese su DNI: ")
-    while not dni.isdigit() or not len(dni)>=6:
+    while not dni.isdigit() or not len(dni) >= 6:
         print("El DNI debe contener solo números.")
         dni = input("Ingrese su DNI: ")
     
-    with open("db/pacientes.csv", 'a', encoding='utf-8') as archivo:
-        archivo.write(f"{dni},{nombre},{apellido}\n")
-    
-    return dni
+    # Verificar existencia
+    # si existe mensaje de bienvenida
+    pacientes = open("db/pacientes.csv", "rt")
+    linea = pacientes.readline()
+    while linea != "": 
+        valores = linea.strip().split(',')
+        if valores[0] == dni:
+            existe = True
+            break
+        else:
+            existe = False
+            linea = pacientes.readline()
 
+    if existe:
+        print(f"\n\nBienvenido {valores[1]} {valores[2]}")
+    else:
+        nombre = input("Ingrese su nombre: ")
+        while not nombre.isalpha():
+            print("El nombre debe contener solo letras.")
+            nombre = input("Ingrese su nombre: ")
+
+        apellido = input("Ingrese su apellido: ")
+        while not apellido.isalpha():
+            print("El apellido debe contener solo letras.")
+            apellido = input("Ingrese su apellido: ")
+
+        print(f"\n\nBienvenido {nombre} {apellido}")
+        archivo = open("db/pacientes.csv", "at")
+        archivo.write(f"{dni},{nombre},{apellido}\n")
+        archivo.close()    
+
+
+    return dni
 
 def solicitarDNI():
     dni = input("Ingrese su DNI: ")
